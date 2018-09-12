c432d880ec21cc0a66a8f6bb3e208abd2d9cf361
harrison8024 <harrison8024@gmail.com>
2018-09-12 14:51:59 -0700

    npm install react router dom and created Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
+import Home from './Home';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Nav />
+                <Nav/>
+                <Route exact path="/"><Home/></Route>
             </div>
         );
     }
